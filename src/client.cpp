6b58c927781dad9adb2c1853a17c75ce47249dc2
Changhua <lichanghua0821@gmail.com>
2022-10-04 21:39:49 +0800

    Initial commit

---
+++ src/client.cpp
@@ -0,0 +1,111 @@
+/*
+RPC Client
+*/
+
+#include <memory>
+
+#include <grpcpp/grpcpp.h>
+
+#include "../proto/demo.grpc.pb.h"
+
+using namespace std;
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using demo::Demo;
+using demo::Msg;
+
+class DemoClient
+{
+public:
+    DemoClient(shared_ptr<Channel> channel)
+        : stub_(Demo::NewStub(channel))
+    {
+    }
+
+    void SetMsgHandleCb(function<void(Msg &)> msg_handle_cb) { GetMessage(msg_handle_cb); }
+
+    void GetMessage(function<void(Msg &)> msg_handle_cb)
+    {
+        class Reader : public grpc::ClientReadReactor<Msg>
+        {
+        public:
+            Reader(Demo::Stub *stub, function<void(Msg &)> msg_handle_cb)
+                : msg_handle_cb_(msg_handle_cb)
+            {
+                stub->async()->GetMessage(&context_, &empty_, this);
+                StartRead(&msg_);
+                StartCall();
+            }
+
+            void OnReadDone(bool ok) override
+            {
+                if (ok) {
+                    try {
+                        msg_handle_cb_(msg_);
+                    } catch (...) {
+                        cout << "OnMsg wrong..." << endl;
+                    }
+                    StartRead(&msg_);
+                }
+            }
+
+            void OnDone(const Status &s) override
+            {
+                unique_lock<mutex> l(mu_);
+                status_ = s;
+                done_   = true;
+                cv_.notify_one();
+            }
+
+            Status Await()
+            {
+                unique_lock<mutex> l(mu_);
+                cv_.wait(l, [this] { return done_; });
+                return move(status_);
+            }
+
+        private:
+            demo::Empty empty_;
+            demo::Msg msg_;
+            ClientContext context_;
+
+            mutex mu_;
+            Status status_;
+            bool done_ = false;
+            condition_variable cv_;
+
+            function<void(Msg &)> msg_handle_cb_;
+        };
+
+        Reader reader(stub_.get(), msg_handle_cb);
+        Status status = reader.Await();
+
+        if (status.ok()) {
+            cout << "GetMessage rpc succeeded." << endl;
+        } else {
+            cout << "GetMessage rpc failed." << endl;
+        }
+    }
+
+private:
+    unique_ptr<Demo::Stub> stub_;
+};
+
+int OnMsg(Msg msg)
+{
+    cout << "Got Message: " << msg.id() << ", " << msg.type() << endl;
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    DemoClient demo(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+
+    function<void(Msg &)> cb = OnMsg;
+    demo.SetMsgHandleCb(cb);
+
+    return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,155 @@
+/*
+Server
+*/
+
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <random>
+#include <string>
+#include <thread>
+
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+
+#include "../proto/demo.grpc.pb.h"
+
+using namespace std;
+
+using grpc::CallbackServerContext;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::Status;
+
+using demo::Demo;
+using demo::Empty;
+using demo::Msg;
+
+mutex gMutex;
+queue<Msg> gMsgQueue;
+condition_variable gCv;
+
+void ProduceMsg(queue<Msg> *msg_queue)
+{
+    random_device rd;  // 随机数生成器
+    mt19937 gen(rd()); // Mersenne Twister 算法
+
+    uniform_int_distribution<int> random_i(1000, 5000); // (1000, 5000) 均匀分布
+
+    int i = 0;
+    while (true) {
+        int tmp = random_i(gen); // 生成 (1000, 5000) 随机数
+
+        // 生成消息
+        Msg msg;
+        msg.set_id("Id_" + to_string(i++));
+        msg.set_type(to_string(tmp));
+
+        // 推送到队列
+        unique_lock<std::mutex> locker(gMutex);
+        msg_queue->push(msg);
+        locker.unlock();
+
+        // 通知各方消息就绪
+        gCv.notify_all();
+
+        // 模拟消息到达间隔
+        sleep(tmp / 1000);
+    }
+}
+
+void ConsumeMsg(queue<Msg> *msg_queue)
+{
+    while (true) {
+        unique_lock<std::mutex> lock(gMutex);
+        gCv.wait(lock, [&] { return !msg_queue->empty(); });
+        auto m = msg_queue->front();
+        msg_queue->pop();
+        lock.unlock();
+
+        cout << m.id() << ": " << m.type() << endl;
+    }
+}
+
+class DemoImpl final : public Demo::CallbackService
+{
+public:
+    explicit DemoImpl(queue<Msg> &q, mutex &m, condition_variable &cv)
+    {
+        msg_q  = &q;
+        msg_m  = &m;
+        msg_cv = &cv;
+    }
+
+    grpc::ServerWriteReactor<Msg> *GetMessage(CallbackServerContext *context, const Empty *empty) override
+    {
+        class Getter : public grpc::ServerWriteReactor<Msg>
+        {
+        public:
+            Getter(queue<Msg> *q, mutex *m, condition_variable *cv)
+                : msg_q_(q)
+                , msg_m_(m)
+                , msg_cv_(cv)
+            {
+                cout << "New Call" << endl;
+                NextWrite();
+            }
+            void OnDone() override { delete this; }
+            void OnWriteDone(bool /*ok*/) override { NextWrite(); }
+
+        private:
+            void NextWrite()
+            {
+                unique_lock<std::mutex> lock(*msg_m_);
+                gCv.wait(lock, [&] { return !msg_q_->empty(); });
+                tmp_ = msg_q_->front();
+                msg_q_->pop();
+                lock.unlock();
+
+                StartWrite(&tmp_);
+
+                // Finish(Status::OK);  // 结束本次通信
+            }
+            Msg tmp_; // 如果将它放到 NextWrite 内部，StartWrite 调用时可能已经出了作用域
+            queue<Msg> *msg_q_;
+            mutex *msg_m_;
+            condition_variable *msg_cv_;
+        };
+
+        return new Getter(msg_q, msg_m, msg_cv);
+    }
+
+private:
+    queue<Msg> *msg_q;
+    mutex *msg_m;
+    condition_variable *msg_cv;
+};
+
+void RunServer()
+{
+    string server_address("localhost:50051");
+    DemoImpl service(gMsgQueue, gMutex, gCv);
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIME_MS, 2000);
+    builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 3000);
+    builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS, 1);
+    builder.RegisterService(&service);
+    unique_ptr<Server> server(builder.BuildAndStart());
+    cout << "Server listening on " << server_address << endl;
+    server->Wait();
+}
+
+int main(int argc, char **argv)
+{
+    thread t1(ProduceMsg, &gMsgQueue); // 模拟生产消息
+    // thread t2(ConsumeMsg, &gMsgQueue); // 验证消费消息
+
+    RunServer();
+
+    return 0;
+}
