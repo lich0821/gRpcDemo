11042c3d052ad536fed094de506ce0f5cd46f70b
Changhua <lichanghua0821@gmail.com>
2022-10-17 07:30:34 +0800

    Enhance python client

--- python/client.py
+++ python/client.py
@@ -3,9 +3,10 @@
 
 import atexit
 import logging
+import re
 from threading import Thread
 from time import sleep
-from typing import Any, Callable, Optional
+from typing import Callable, List, Optional
 
 import grpc
 
@@ -14,12 +15,47 @@ import demo_pb2_grpc
 
 
 class Demo():
+    class WxMsg():
+        def __init__(self, msg):
+            self._is_self = msg.is_self
+            self._is_group = msg.is_group
+            self.type = msg.type
+            self.id = msg.id
+            self.xml = msg.xml
+            self.sender = msg.sender
+            self.roomid = msg.roomid
+            self.content = msg.content
+
+        def from_self(self):
+            """是否自己发的消息"""
+            return self._is_self == 1
+
+        def from_group(self):
+            """是否群聊消息"""
+            return self._is_group
+
+        def is_at(self, wxid):
+            """群消息，在@名单里，并且不是@所有人"""
+            return self.from_group() and re.findall(
+                f"<atuserlist>.*({wxid}).*</atuserlist>", self.xml) and not re.findall(r"@(?:所有人|all)", self.xml)
+
+        def is_text(self):
+            """是否文本消息"""
+            return self.type == 1
+
     def __init__(self, host_port: str = "localhost:10086") -> None:
         self._enable_recv_msg = False
         self.LOG = logging.getLogger("WCF")
         self._channel = grpc.insecure_channel(host_port)
         self._stub = demo_pb2_grpc.DemoStub(self._channel)
         atexit.register(self.disable_recv_msg)
+        self.contacts = []
+        self._SQL_TYPES = {
+            0: lambda x: x.decode("utf-8"),
+            1: lambda x: x.decode("utf-8"),
+            2: lambda x: x.decode("utf-8"),
+            3: lambda x: x.decode("utf-8"),
+            4: lambda x: x.decode("utf-8")}
 
     def __del__(self):
         self.disable_recv_msg()
@@ -41,13 +77,13 @@ class Demo():
         rsps = self._stub.RpcEnableRecvMsg(demo_pb2.Empty())
         try:
             for rsp in rsps:
-                func(rsp)
+                func(self.WxMsg(rsp))
         except Exception as e:
             self.LOG.error(f"RpcEnableRecvMsg: {e}")
         finally:
             self.disable_recv_msg()
 
-    def enable_recv_msg(self, callback: Callable[..., Any] = None) -> bool:
+    def enable_recv_msg(self, callback: Callable[[WxMsg], None] = None) -> bool:
         if self._enable_recv_msg:
             return True
 
@@ -81,25 +117,42 @@ class Demo():
         rsp = self._stub.RpcSendImageMsg(demo_pb2.ImageMsg(path=path, receiver=receiver))
         return rsp.status
 
-    def get_msg_types(self) -> demo_pb2.MsgTypes:
+    def get_msg_types(self) -> dict:
         rsp = self._stub.RpcGetMsgTypes(demo_pb2.Empty())
-        return rsp
+        return dict(sorted(dict(rsp.types).items()))
 
-    def get_contacts(self) -> demo_pb2.Contacts:
+    def get_contacts(self) -> List[dict]:
         rsp = self._stub.RpcGetContacts(demo_pb2.Empty())
-        return rsp
+        for cnt in rsp.contacts:
+            gender = ""
+            if cnt.gender == 1:
+                gender = "男"
+            elif cnt.gender == 2:
+                gender = "女"
+            self.contacts.append({"wxid": cnt.wxid, "code": cnt.code, "name": cnt.name,
+                                 "country": cnt.country, "province": cnt.province, "city": cnt.city, "gender": gender})
+        return self.contacts
 
-    def get_dbs(self) -> demo_pb2.DbNames:
+    def get_dbs(self) -> List[str]:
         rsp = self._stub.RpcGetDbNames(demo_pb2.Empty())
-        return rsp
+        return rsp.names
 
-    def get_tables(self, db: str) -> demo_pb2.DbTables:
+    def get_tables(self, db: str) -> List[dict]:
+        tables = []
         rsp = self._stub.RpcGetDbTables(demo_pb2.String(str=db))
-        return rsp
+        for tbl in rsp.tables:
+            tables.append({"name": tbl.name, "sql": tbl.sql.replace("\t", "")})
+        return tables
 
-    def query_sql(self, db: str, sql: str) -> demo_pb2.DbRows:
+    def query_sql(self, db: str, sql: str) -> List[dict]:
+        result = []
         rsp = self._stub.RpcExecDbQuery(demo_pb2.DbQuery(db=db, sql=sql))
-        return rsp
+        for r in rsp.rows:
+            row = {}
+            for f in r.fields:
+                row[f.column] = self._SQL_TYPES[f.type](f.content)
+            result.append(row)
+        return result
 
     def accept_new_friend(self, v3: str, v4: str) -> int:
         rsp = self._stub.RpcAcceptNewFriend(demo_pb2.Verification(v3=v3, v4=v4))
--- python/main.py
+++ python/main.py
@@ -4,13 +4,22 @@
 from client import Demo
 
 
+def func(msg: Demo.WxMsg):
+    if msg.from_group():
+        s = f"【{msg.roomid}】的 {msg.sender} {'@你'if msg.is_at('me') else '发来的消息'}：\n"
+    else:
+        s = f"好友 {msg.sender} 发来的消息：\n"
+
+    s += msg.content
+    print(s)
+
+
 def main():
     wcf = Demo()
-
     print(f"Is Login: {True if wcf.is_login() else False}")
     print(f"SelfWxid: {wcf.get_self_wxid()}")
 
-    wcf.enable_recv_msg(print)
+    wcf.enable_recv_msg(func)
     # wcf.disable_recv_msg() # Call anytime when you don't want to receive messages
     wcf.send_text("Hello world.", "wxid_to_send")
     wcf.send_image("image path", "yuwangdadi")
